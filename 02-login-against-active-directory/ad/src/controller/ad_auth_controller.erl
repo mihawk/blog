e24f7ca1930b0f5ee4ae8f4cc9ed2e4f616e0243
chan <chan.sisowath@gmail.com>
2015-09-17 17:58:01 +0800

    Update ad_auth_controller.erl

--- 02-login-against-active-directory/ad/src/controller/ad_auth_controller.erl
+++ 02-login-against-active-directory/ad/src/controller/ad_auth_controller.erl
@@ -15,12 +15,10 @@ login('POST', []) ->
   Password = proplists:get_value("password", Params),
 
   %% check against active directory
-  lager:info("Login: ~p, Password: ~p",[Login, Password]),
   case eldap_api:check_password(default, list_to_binary(Login), list_to_binary(Password)) of
        true -> 
                %%if user is authentified, link is current session
                boss_session:set_session_data(Sid, user, Login),
-               %%Redirect = boss_session:get_session_data(Sid, redirect_after_login_if_success),
                lager:info("success login ~p....",[Login]),
                {ok, [{success_msg, "You are authetified"}]};
        false ->
